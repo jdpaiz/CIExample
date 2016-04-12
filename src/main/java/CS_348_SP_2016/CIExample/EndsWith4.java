9c60b6b3432f690749d9d133ac57e9c7b83a0cfd
Karl R. Wurst <karl@w-sts.com>
2016-04-12 07:39:22 -0400

    Initial commit with code, JUnit test code, Maven configuration, Travis configuration

---
+++ src/main/java/CS_348_SP_2016/CIExample/EndsWith4.java
@@ -0,0 +1,18 @@
+package CS_348_SP_2016.CIExample;
+
+public class EndsWith4 {
+	
+	String names;
+
+	public EndsWith4()  {
+		names = "";
+		// add your name here in the form of a line that looks like:
+		// names+= "John Smith\n";
+	}
+
+	@Override
+	public String toString() {
+		return "EndsWith4 [names=" + names + "]";
+	}
+
+}
---
+++ src/test/java/CS_348_SP_2016/CIExample/EndsWith5Test.java
@@ -0,0 +1,15 @@
+package CS_348_SP_2016.CIExample;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EndsWith5Test {
+
+	@Test
+	public void testToString() {
+		EndsWith5 five = new EndsWith5();
+		assertEquals("EndsWith5 [names=]", five.toString());
+	}
+
+}
