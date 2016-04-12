0a1c42419ca5e2143510e024902e4c61b19bb0e7
Karl R. Wurst <karl@w-sts.com>
2016-04-12 16:40:53 -0400

    Clean up incorrectly pushed conflicted files

--- src/main/java/CS_348_SP_2016/CIExample/EndsWith0.java
+++ src/main/java/CS_348_SP_2016/CIExample/EndsWith0.java
@@ -6,20 +6,15 @@ public class EndsWith0 {
 
 	public EndsWith0()  {
 		names = "";
-<<<<<<< HEAD
 		names += "Xavier Giles\n";
-		// add your name here in the form of a line that looks 
-		//like
-=======
 		names += "Ryan Murphy\n";
 		// add your name here in the form of a line that looks like:
->>>>>>> 82e035623a0fce4f1a5568fe44f575b277c19d32
 		// names+= "John Smith\n"
 	}
 
 	@Override
 	public String toString() {
-		return "EndsWith0 [names= "+ names +"]";
+		return "EndsWith0 [names="+ names +"]";
 	}
 
 }
