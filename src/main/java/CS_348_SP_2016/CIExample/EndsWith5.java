7a4965f411d194bb6d8188c7079a78945ee33dff
Jonathan Paiz <jdpaiz@aol.com>
2016-04-21 11:40:16 -0400

    Jonathan Paiz changes

--- src/main/java/CS_348_SP_2016/CIExample/EndsWith5.java
+++ src/main/java/CS_348_SP_2016/CIExample/EndsWith5.java
@@ -1,13 +1,14 @@
 package CS_348_SP_2016.CIExample;
 
 public class EndsWith5 {
-	
+
 	String names;
 
 	public EndsWith5()  {
 		names = "";
 		names += "Daniel Mahoney\n";
 		names += "Kyle Polewaczyk\n";
+		names += "Jonathan Paiz\n";
 		// add your name here in the form of a line that looks like:
 		// names+= "John Smith\n";
 	}
